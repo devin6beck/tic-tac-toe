9e693e08c85bb5e4b5667c8959282e24cd66cea8
Devin Beck <devin6beck@gmail.com>
2022-02-25 12:16:20 -0700

    cleanup function winnerCheck

--- script.js
+++ script.js
@@ -181,23 +181,14 @@ const gameBoard = ((player1, player2, playHuman, playEasy) => {
   }
 
   function winnerCheck(player) {
-    if (board[0] === player && board[1] === player && board[2] === player){
-      return true;
-    } else if (board[3] === player && board[4] === player && board[5] === player){
-      return true;
-    } else if (board[6] === player && board[7] === player && board[8] === player){
-      return true;
-    } else if (board[0] === player && board[3] === player && board[6] === player){
-      return true;
-    } else if (board[1] === player && board[4] === player && board[7] === player){
-      return true;
-    } else if (board[2] === player && board[5] === player && board[8] === player){
-      return true;
-    } else if (board[0] === player && board[4] === player && board[8] === player){
-      return true;
-    } else if (board[2] === player && board[4] === player && board[6] === player){
-      return true;
-    }
+    if (board[0] === player && board[1] === player && board[2] === player)return true;
+    if (board[3] === player && board[4] === player && board[5] === player)return true; 
+    if (board[6] === player && board[7] === player && board[8] === player)return true;
+    if (board[0] === player && board[3] === player && board[6] === player)return true; 
+    if (board[1] === player && board[4] === player && board[7] === player)return true;  
+    if (board[2] === player && board[5] === player && board[8] === player)return true;
+    if (board[0] === player && board[4] === player && board[8] === player)return true;  
+    if (board[2] === player && board[4] === player && board[6] === player)return true;    
   }
 
 })
