c628ace02d37142c99d8505fd1941a44305ce389
Devin Beck <devin6beck@gmail.com>
2022-02-26 08:26:13 -0700

    refactor winnerCheck

--- script.js
+++ script.js
@@ -181,14 +181,16 @@ const gameBoard = ((player1, player2, playHuman, playEasy) => {
   }
 
   function winnerCheck(player) {
-    if (board[0] === player && board[1] === player && board[2] === player)return true;
-    if (board[3] === player && board[4] === player && board[5] === player)return true; 
-    if (board[6] === player && board[7] === player && board[8] === player)return true;
-    if (board[0] === player && board[3] === player && board[6] === player)return true; 
-    if (board[1] === player && board[4] === player && board[7] === player)return true;  
-    if (board[2] === player && board[5] === player && board[8] === player)return true;
-    if (board[0] === player && board[4] === player && board[8] === player)return true;  
-    if (board[2] === player && board[4] === player && board[6] === player)return true;    
+    if (
+      (board[0] === player && board[1] === player && board[2] === player) ||
+      (board[3] === player && board[4] === player && board[5] === player) || 
+      (board[6] === player && board[7] === player && board[8] === player) || 
+      (board[0] === player && board[3] === player && board[6] === player) || 
+      (board[1] === player && board[4] === player && board[7] === player) ||  
+      (board[2] === player && board[5] === player && board[8] === player) || 
+      (board[0] === player && board[4] === player && board[8] === player) ||   
+      (board[2] === player && board[4] === player && board[6] === player)
+    ) return true;     
   }
 
 })
